a0875342d333ced12da1071b4365dc8429c0e8be
cheesss <90661454+cheesss@users.noreply.github.com>
2022-04-02 11:51:01 +0900

    과제

--- anci_test/14_3hw.c
+++ anci_test/14_3hw.c
@@ -14,13 +14,30 @@ typedef node* link;
 void input(link);
 link find(link, char);
 link delete(link);
+void delete_all(link);
+void print(link);
 
 main() {
 	link head;
 	head = (link)malloc(sizeof(node));
 	input(head);
 
+	print(head);
+
 	delete(head);
+
+	print(head);
+
+	printf("리스트 전체를 지우려면 1을 누르십시오\n");
+	int i;
+	scanf("%d", &i);
+	if (i == 1)
+	{
+		delete_all(head);
+		printf("모두 지워졌습니다.");
+	}
+	else
+		printf("리스트를 지우지 않았습니다.");
 
 	free(head);
 }
@@ -28,7 +45,7 @@ main() {
 
 void input(link head) {
 
-	printf("숫자를 입력하시오, 노드 생성을 완료하려면 E를 입력하시오:");
+	printf("알파벳을 입력하시오, 노드 생성을 완료하려면 E를 입력하시오:");
 	scanf("%c", &head->c);
 	getchar();
 
@@ -48,7 +65,7 @@ link find(link head, char key) {
 		return(head);
 	}
 	else if (head->next->next == NULL) {
-		return(NULL);
+		return(head);
 	}
 	else {
 		return(find(head->next, key));
@@ -61,14 +78,15 @@ link delete(link head) {
 	link n1, p;
 	printf("지우고 싶은 알파벳을 입력하시오: ");
 	scanf("%c", &key);
-	getchar();
 
 	n1 = find(head, key);
 	if (n1 == NULL) {
 		printf("찾으려는 값이 없습니다.");
 	}
-	else if (head->c == key) {
-		head = head->next;
+	else if (n1->c == key) {
+		link p = n1;
+		p = n1->next;
+		free(n1);
 	}
 	else {
 		p = n1->next->next;
@@ -78,3 +96,19 @@ link delete(link head) {
 	return(head);
 }
 
+void print(link head) {
+	if (head != NULL) {
+		printf("%c", head->c);
+		print(head->next);
+	}
+}
+
+void delete_all(link head) {
+	link s;
+	link t = head->next;
+	while (t != NULL) {
+		s = t;
+		t = t->next;
+		free(s);
+	}
+}
