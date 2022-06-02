ff715c99c8942ca7c9adedc61813c62edcc09201
cheesss <90661454+cheesss@users.noreply.github.com>
2022-06-02 11:52:24 +0900

    생성자 상속 수업

--- anci_test/포인터상속4.cpp
+++ anci_test/포인터상속4.cpp
@@ -6,7 +6,7 @@ protected:
 	double real;
 public:
 	Real() { cout << "real constructor" << endl; }
-	~Real() { cout << "real destrucor" << endl; }
+	virtual ~Real() { cout << "real destrucor" << endl; }	//자식 소멸자가 불러와져서 깔끔하게 지울수 있도록 virtual 해준다
 	void setval(double real) { this->real = real; }
 	virtual void display() { cout << "real value: " << this->real << endl; }
 };
@@ -16,7 +16,7 @@ protected:
 	double imag;
 public:
 	Image() { cout << "image constructor" << endl; }
-	~Image() { cout << "image destructor" << endl; }
+	virtual ~Image() { cout << "image destructor" << endl; }
 	void setval(double imag) { this->imag = imag; }
 	void display() { cout << "imag value: " << this->imag << "i" << endl; }
 };
@@ -35,6 +35,6 @@ public:
 };
 
 int main() {	//virtual을 사용하면 부모클래스가 자식클래스를 가리켜도 자식클래스 멤버를 사용할 수 있게해준다.
-	Complex* cp = new Complex;
-	delete cp;
+	Real* rp = new Complex;	//불러올때는 상속받은 부모, 자식 클래스 모두 생성
+	delete rp;	//반환할때는 부모클래스만 반환 -> 나머지는 남아있다.
 }//포인터만 선언하면 객체 공간이 할당되지 않으므로, 선언하는것만으로는 생성자가 호출되지 않는다.
